29fda3425f357d756f419be916085d1c6d380cf5
Gustavo Inglês <98420656+gustavoingles@users.noreply.github.com>
2024-09-25 08:52:05 -0300

    Soluções para os exercícios de estrutura de dados

---
+++ exercicio4_arvore_pre_ordem.py
@@ -0,0 +1,7 @@
+#Exercício 04
+
+class No:
+    def __init__(self, valor=0, esquerda=None, direita=None):
+        self.valor = valor
+        self.esquerda = esquerda
+        self.direita = direita

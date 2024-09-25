b8670effca1547b49b4701747f9514481a646420
André Lucas <124175033+LuciosSB@users.noreply.github.com>
2024-09-25 08:56:19 -0300

    Add files via upload

---
+++ TesteEstruturadeDados-Andre-Lucas/fila.py
@@ -0,0 +1,6 @@
+def fila_estranha():
+    fila = []
+    fila.append(1)
+    fila.append(2)
+    fila.pop()
+    return fila[]
---
+++ TesteEstruturadeDados-Andre-Lucas/fila_inverter.py
@@ -0,0 +1,7 @@
+fila = []
+fila.append(1)
+fila.append(2)
+fila.append(3)
+final = []
+while fila:
+    final.append(fila.pop())

d6128e42a7c0ba82dd787f727d195703bef22f5e
Thiago Lucas <163213318+DevThiagoLucas@users.noreply.github.com>
2024-09-25 09:38:39 -0300

    Update exercicio2_fila_simulacao.py

--- exercicio2_fila_simulacao.py
+++ exercicio2_fila_simulacao.py
@@ -1,1 +1,27 @@
+class Fila:
 
+    def __init__ (self):
+      self.itens = []
+
+    def enfileirar(self,valor):
+       self.itens.append(valor)
+
+    def desenfileirar(self):
+      if len(self.itens) == 0:
+        print('Fila Vazia')
+        return None
+
+      else:
+        print(f'O número retirado foi {self.itens[0]}')
+        return self.itens.pop(0)
+
+  
+
+
+fila = Fila()
+
+fila.enfileirar(1)
+fila.enfileirar(2)
+print(fila.itens)
+fila.desenfileirar()
+print(fila.itens)

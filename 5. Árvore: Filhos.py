6e1fd9864e767b0ea8b64db1c05ec9ff3a26c292
Guilherme D. Possas <possasg@gmail.com>
2024-09-25 08:31:24 -0300

    add: missing files

--- 5. Árvore: Filhos.py
+++ 5. Árvore: Filhos.py
@@ -1,3 +1,5 @@
+from BinaryTree import BinaryTree
+
 def find_childrens(root, val):
   if (node := _find_childrens(root, val)):
     output = []
@@ -12,13 +14,25 @@ def _find_childrens(node, val):
   if node.val == val:
     return node
   
-  return _find_childrens(node.left) or _find_childrens(node.right)
+  return _find_childrens(node.left, val) or _find_childrens(node.right, val)
 
 def pre_order(node, output):
   if not node:
     return
   
   output.append(node.val)
-  pre_order(node.left)
-  pre_order(node.right)
+  pre_order(node.left, output)
+  pre_order(node.right, output)
 
+bt = BinaryTree()
+
+bt.add(50)
+bt.add(70)
+bt.add(35)
+bt.add(22)
+bt.add(21)
+bt.add(23)
+bt.add(36)
+bt.add(37)
+
+print(find_childrens(bt.root, 35))

ab9f1a4d2a28bf559a8a58943b1cca1fce9c4a87
Thiago Lucas <163213318+DevThiagoLucas@users.noreply.github.com>
2024-09-25 09:39:37 -0300

    Update exercicio4_arvore_pre_ordem.py

--- exercicio4_arvore_pre_ordem.py
+++ exercicio4_arvore_pre_ordem.py
@@ -1,1 +1,11 @@
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
 
+raiz = No(10)
+raiz.esquerda = No(5)
+raiz.direita = No(15)
+raiz.esquerda.esquerda = No(2)
+raiz.esquerda.direita = No(7)
